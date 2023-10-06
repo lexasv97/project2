775ee34e43b8bbc73dc8a0443176b7c037e37dde
Aleksei Seletckii <alexey.sv.97@gmail.com>
2023-10-06 12:50:52 -0400

    32th commit

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       secure: process.env.NODE_ENV === 'production',
       httpOnly: true,
-      maxAge: 60000 // 60 * 1000 ms === 1 min
+      maxAge: 600000 // 60 * 1000 ms === 1 min
     }, // ADDED code below !!!
     store: MongoStore.create({
       mongoUrl: process.env.MONGODB_URI 
