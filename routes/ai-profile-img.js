64c40c02e391372920ec3c807b1025709c9cc431
Aleksei Seletckii <alexey.sv.97@gmail.com>
2023-10-06 13:48:44 -0400

    38th commit

--- routes/ai-profile-img.js
+++ routes/ai-profile-img.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 const Creator = require('../models/Creator');
 var router = express.Router();
 // api key goes to .env
-const API_KEY = "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"
+const API_KEY = "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"
 
 const { isCreatorLoggedIn } = require('../middleware/creator-route-guard');
 const {isUserLoggedIn} = require('../middleware/user-route-guard')
